84628628fe314f5339bfd382965c24865f1741c0
trisrosco <trisrosco@gmail.com>
2023-11-08 16:38:48 +0000

    added placeholder contact form

--- cms/src/App.js
+++ cms/src/App.js
@@ -1,9 +1,11 @@
-
 import './App.css';
+import React from 'react';
+import ContactForm from './components/ContactForm';
 
 function App() {
   return (
     <div className="App">
+      <ContactForm />
 
     </div>
   );
