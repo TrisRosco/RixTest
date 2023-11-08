6855d6f44ec5a5537d9f3ad3b17fc46ed6f0682e
trisrosco <trisrosco@gmail.com>
2023-11-08 18:26:08 +0000

    laid out foundations for search bar

--- cms/src/Models/queries.js
+++ cms/src/Models/queries.js
@@ -1,33 +1,23 @@
-import { supabase } from './client';
+import { supabase } from "./client";
 
-const table = 'contacts';
+const table = "contacts";
 
 export const getContacts = async () => {
-  const { data, error } = await supabase
-    .from(table)
-    .select('*');
+  const { data, error } = await supabase.from(table).select("*");
   if (error) throw new Error(error.message);
   return data;
 };
 
 export const addEditContact = async (contact) => {
   const { data, error } = contact.id
-    ? await supabase
-        .from(table)
-        .update(contact)
-        .match({ id: contact.id })
-    : await supabase
-        .from(table)
-        .insert([contact]);
+    ? await supabase.from(table).update(contact).match({ id: contact.id })
+    : await supabase.from(table).insert([contact]);
   if (error) throw new Error(error.message);
   return data;
 };
 
 export const deleteContact = async (id) => {
-  const { data, error } = await supabase
-    .from(table)
-    .delete()
-    .match({ id });
+  const { data, error } = await supabase.from(table).delete().match({ id });
   if (error) throw new Error(error.message);
   return data;
 };
