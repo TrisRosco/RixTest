8b84825a09c4992e9cd818aac5480e7b399c5bac
trisrosco <trisrosco@gmail.com>
2023-11-08 16:46:44 +0000

    added basic form functionality

--- cms/src/components/ContactForm/index.js
+++ cms/src/components/ContactForm/index.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useState } from "react";
+import { TextField, Button } from "@mui/material";
 
-const ContactForm = () => {
-    return (
-        <div>
-        <h1>Contact Form</h1>
-        </div>
-    );
-    }
+const ContactForm = ({ onSubmit, initialData = {} }) => {
+  const [contact, setContact] = useState("");
+
+  const handleChange = (e) => {
+    setContact({ ...contact, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSubmit(contact);
+      }}
+    >
+      <TextField
+        label="Name"
+        name="name"
+        value={contact.name}
+        onChange={handleChange}
+        required
+      />
+      <TextField
+        label="Email"
+        name="email"
+        value={contact.email}
+        onChange={handleChange}
+        required
+      />
+      <TextField
+        label="Phone Number"
+        name="phone"
+        value={contact.phone}
+        onChange={handleChange}
+        required
+      />
+
+      <Button variant="contained" color="primary" type="submit">
+        Save Contact
+      </Button>
+    </form>
+  );
+};
 
 export default ContactForm;
