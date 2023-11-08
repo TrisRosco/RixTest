2e5a02a1977245f664c0d4f165477b37d02689f9
trisrosco <trisrosco@gmail.com>
2023-11-08 18:30:42 +0000

    added tries and catches to queries, along with logs and errors

--- cms/src/Models/queries.js
+++ cms/src/Models/queries.js
@@ -3,21 +3,39 @@ import { supabase } from "./client";
 const table = "contacts";
 
 export const getContacts = async () => {
-  const { data, error } = await supabase.from(table).select("*");
-  if (error) throw new Error(error.message);
-  return data;
+  try {
+    const { data, error } = await supabase.from(table).select("*");
+    if (error) throw new Error(error.message);
+    console.log("Contacts fetched:", data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching contacts:", error);
+    throw error; 
+  }
 };
 
 export const addEditContact = async (contact) => {
-  const { data, error } = contact.id
-    ? await supabase.from(table).update(contact).match({ id: contact.id })
-    : await supabase.from(table).insert([contact]);
-  if (error) throw new Error(error.message);
-  return data;
+  try {
+    const { data, error } = contact.id
+      ? await supabase.from(table).update(contact).match({ id: contact.id })
+      : await supabase.from(table).insert([contact]);
+    if (error) throw new Error(error.message);
+    console.log("Contact added/edited:", data);
+    return data;
+  } catch (error) {
+    console.error("Error adding/editing contact:", error);
+    throw error; 
+  }
 };
 
 export const deleteContact = async (id) => {
-  const { data, error } = await supabase.from(table).delete().match({ id });
-  if (error) throw new Error(error.message);
-  return data;
+  try {
+    const { data, error } = await supabase.from(table).delete().match({ id });
+    if (error) throw new Error(error.message);
+    console.log("Contact deleted:", data);
+    return data;
+  } catch (error) {
+    console.error("Error deleting contact:", error);
+    throw error; 
+  }
 };
