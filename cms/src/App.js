d9f5ee1c68bc51a917292b3bfd8f1e356eb9ef62
trisrosco <trisrosco@gmail.com>
2023-11-09 10:37:44 +0000

    installed icons, started fleshing out contactList

--- cms/src/App.js
+++ cms/src/App.js
@@ -1,23 +1,36 @@
-import './App.css';
-import React from 'react';
-import ContactForm from './components/ContactForm';
-import SearchBar from './components/SearchBar';
-import { Paper } from '@mui/material';
+import "./App.css";
+import React from "react";
+import ContactForm from "./components/ContactForm";
+import SearchBar from "./components/SearchBar";
+import ContactList from "./components/ContactList";
+import { Paper } from "@mui/material";
 
 function App() {
-
   const handleSubmit = (contact) => {
     console.log(contact);
   };
-
 
   return (
     <div className="App">
       <Paper elevation={3} className="container">
         <ContactForm onSubmit={handleSubmit} />
-        </Paper>
-        <SearchBar onSearch={(value) => console.log(value)} />
-
+      </Paper>
+      <SearchBar onSearch={(value) => console.log(value)} />
+      <ContactList
+        contacts={[
+          {
+            id: 1,
+            name: "Test Name1",
+            email: "Email@email.com",
+          },
+          {
+            id: 2,
+            name: "Test Name2",
+            email: "Email@email.com",
+          },
+        ]}
+        onDelete={(id) => console.log(id)}
+      />
     </div>
   );
 }
--- cms/src/components/ContactList/index.js
+++ cms/src/components/ContactList/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
+import { List, ListItem, ListItemText, IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-const ContactList = () => {
+const ContactList = ({ contacts, onDelete }) => {
   return (
-    <div>
-    </div>
+    <List>
+      {contacts.map((contact) => (
+        <ListItem
+          key={contact.id}
+          secondaryAction={
+            <IconButton
+              edge="end"
+              aria-label="delete"
+              onClick={() => onDelete(contact.id)}
+            >
+              <DeleteIcon />
+            </IconButton>
+          }
+        >
+          <ListItemText primary={contact.name} secondary={{contact.email} ' - ' {contact.phone}} />
+        </ListItem>
+      ))}
+    </List>
   );
 };
 
