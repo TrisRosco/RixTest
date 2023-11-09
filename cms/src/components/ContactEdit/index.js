35aa77438054a0fba01949e65c2debea2b46a06d
trisrosco <trisrosco@gmail.com>
2023-11-09 15:19:53 +0000

    copied input validation and formatting to ContactEdit from ContactForm

--- cms/src/components/ContactEdit/index.js
+++ cms/src/components/ContactEdit/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Dialog,
   DialogTitle,
@@ -8,50 +8,118 @@ import {
   TextField,
 } from "@mui/material";
 
-const ContactEdit = ({ isOpen, onClose, onSave }) => {
+const ContactEdit = ({ isOpen, onClose, onSave, initialData = {} }) => {
+  const [contact, setContact] = useState({
+    name: initialData.name || "",
+    email: initialData.email || "",
+    phone: initialData.phone || "",
+    address: initialData.address || "",
+    postcode: initialData.postcode || "",
+  });
+  const [errors, setErrors] = useState({}); // tracks errors for each field
+
+  const validateInput = (name, value) => {
+    switch (name) {
+      case "name":
+        // this is never triggered because of maxLength
+        return value.length <= 50 ? "" : "Name cannot exceed 50 characters.";
+      case "email":
+        return /\S+@\S+\.\S+/.test(value) ? "" : "Please enter a valid email.";
+      case "phone":
+        return /^\+?(\d{1,3})?\s?-?\d+$/.test(value)
+          ? ""
+          : "Please enter a valid phone number.";
+      case "postcode":
+        // regex for UK postcodes
+        return /^[A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}$/.test(value.toUpperCase())
+          ? ""
+          : "Please enter a valid UK postcode.";
+      default:
+        return "";
+    }
+  };
+
+  const formatPostcode = (value) => {
+    // format postcode to uppercase and add space before last 3 characters if not already there
+    const formatted = value.toUpperCase();
+    if (formatted.length > 3 && formatted[formatted.length - 4] !== " ") {
+      return formatted.slice(0, -3) + " " + formatted.slice(-3);
+    }
+    return formatted;
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const error = validateInput(name, value); // validate input
+    setErrors({ ...errors, [name]: error }); // update errors
+    setContact({ ...contact, [name]: value }); // update contact
+  };
+
   return (
     <Dialog open={isOpen}>
       <DialogTitle>Edit Contact</DialogTitle>
       <DialogContent>
         <TextField
           margin="dense"
+          fullWidth
           label="Name"
-          type="text"
-          fullWidth
-          variant="outlined"
           name="name"
+          value={contact.name}
+          onChange={handleChange}
+          error={!!errors.name}
+          helperText={errors.name}
+          required
+          inputProps={{ maxLength: 50 }}
         />
         <TextField
           margin="dense"
+          fullWidth
           label="Email"
-          type="email"
-          fullWidth
-          variant="outlined"
           name="email"
+          value={contact.email}
+          onChange={handleChange}
+          error={!!errors.email}
+          helperText={errors.email}
+          required
+          inputProps={{ maxLength: 100 }}
         />
         <TextField
           margin="dense"
-          label="Phone"
-          type="tel"
           fullWidth
-          variant="outlined"
+          label="Phone Number"
           name="phone"
+          value={contact.phone}
+          onChange={handleChange}
+          error={!!errors.phone}
+          helperText={errors.phone}
+          required
+          inputProps={{ maxLength: 15 }}
         />
         <TextField
           margin="dense"
+          fullWidth
           label="Address"
-          type="text"
-          fullWidth
-          variant="outlined"
           name="address"
+          value={contact.address}
+          onChange={handleChange}
+          inputProps={{ maxLength: 255 }}
         />
         <TextField
           margin="dense"
-          label="Postcode"
-          type="text"
           fullWidth
-          variant="outlined"
+          label="Postcode"
           name="postcode"
+          value={contact.postcode}
+          onChange={handleChange}
+          error={!!errors.postcode}
+          helperText={errors.postcode}
+          required
+          inputProps={{ maxLength: 7 }}
+          onBlur={(e) => {
+            // Format the postcode
+            const formattedPostcode = formatPostcode(e.target.value);
+            setContact({ ...contact, postcode: formattedPostcode });
+          }}
         />
       </DialogContent>
       <DialogActions>
