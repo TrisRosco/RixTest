87b3feaddcd026ea31f89fe2fae0a431c5495078
trisrosco <trisrosco@gmail.com>
2023-11-09 13:25:11 +0000

    Added a selector and accompanying usestate to the searchbar

--- cms/src/components/SearchBar/index.js
+++ cms/src/components/SearchBar/index.js
@@ -1,16 +1,50 @@
-import React from 'react';
-import { TextField } from '@mui/material';
+import React, { useState } from "react";
+import {
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material";
 
 const SearchBar = ({ onSearch }) => {
+  // usestate to track the selected field
+  const [searchField, setSearchField] = useState("name");
+
+  // Handler for the select field change
+  const handleFieldChange = (event) => {
+    setSearchField(event.target.value);
+  };
+
+  // Handler for the search field change
+  const handleSearchChange = (event) => {
+    // Pass both the field and value up
+    onSearch(event.target.value, searchField);
+  };
+
   return (
-    <TextField
-      type="text"
-      onChange={(e) => onSearch(e.target.value)}
-      label="Search contacts..."
-      variant="outlined"
-      fullWidth
-      margin="normal"
-    />
+      <FormControl>
+        <InputLabel id="search-field-label">Search By</InputLabel>
+        <Select
+          labelId="search-field-label"
+          value={searchField}
+          label="Search By"
+          onChange={handleFieldChange}
+        >
+          <MenuItem value="name">Name</MenuItem>
+          <MenuItem value="email">Email</MenuItem>
+          <MenuItem value="address">Address</MenuItem>
+          <MenuItem value="postcode">Postcode</MenuItem>
+          <MenuItem value="phone">Number</MenuItem>
+        </Select>
+
+        <TextField
+          type="text"
+          onChange={handleSearchChange}
+          label="Search contacts..."
+          variant="outlined"
+        />
+      </FormControl>
   );
 };
 
