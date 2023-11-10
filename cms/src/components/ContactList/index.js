e69d083eb3ab971a31715a618d538dfca52b2447
trisrosco <trisrosco@gmail.com>
2023-11-10 16:24:53 +0000

    added dividers between entrys, had to put them in a react fragment

--- cms/src/components/ContactList/index.js
+++ cms/src/components/ContactList/index.js
@@ -5,59 +5,56 @@ import {
   ListItemText,
   IconButton,
   Tooltip,
+  Divider,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit"; // Import the edit icon
 
 const ContactList = ({ contacts, onDelete, onEdit }) => {
-
-
   return (
     <List>
       {contacts.map((contact) => (
-        <ListItem
-          key={contact.id}
-          secondaryAction={
-            <>
-              <Tooltip title="Edit Contact">
-                <IconButton
-                  aria-label="edit"
-                  onClick={() => onEdit(contact)}
-                >
-                  <EditIcon />
-                </IconButton>
-              </Tooltip>
-              <Tooltip title="Remove Contact">
-                <IconButton
-                  edge="end"
-                  aria-label="delete"
-                  onClick={() => onDelete(contact.id)}
-                >
-                  <DeleteIcon />
-                </IconButton>
-              </Tooltip>
-            </>
-          }
-        >
-          <ListItemText
-            primary={contact.name}
-            secondary={
+        <React.Fragment key={contact.id}>
+          <ListItem
+            secondaryAction={
               <>
-                {"Email: " + contact.email}
-                <br />
-                {"Phone: " + contact.phone}
-                <br />
-                {"Address: " + contact.address + ", " + contact.city}
-                <br />
-                {"Postcode: " + contact.postcode}
+                <Tooltip title="Edit Contact">
+                  <IconButton aria-label="edit" onClick={() => onEdit(contact)}>
+                    <EditIcon />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title="Remove Contact">
+                  <IconButton
+                    edge="end"
+                    aria-label="delete"
+                    onClick={() => onDelete(contact.id)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </Tooltip>
               </>
             }
-          />
-        </ListItem>
+          >
+            <ListItemText
+              primary={contact.name}
+              secondary={
+                <>
+                  {"Email: " + contact.email}
+                  <br />
+                  {"Phone: " + contact.phone}
+                  <br />
+                  {"Address: " + contact.address + ", " + contact.city}
+                  <br />
+                  {"Postcode: " + contact.postcode}
+                </>
+              }
+            />
+          </ListItem>
+          <Divider />
+        </React.Fragment>
       ))}
     </List>
   );
 };
 
 export default ContactList;
-
